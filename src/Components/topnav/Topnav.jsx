f641e4db14a4c6314a7df9d9bd9ae2864a6f7f4d
Supamit2545 <mairutv@gmail.com>
2024-07-10 23:52:20 +0700

    Second

--- src/Components/topnav/Topnav.jsx
+++ src/Components/topnav/Topnav.jsx
@@ -17,16 +17,16 @@ const Topnav = () => {
             <img src="" alt="" />
         </div>  
         <div className='topnav-center'>
-            <ul className='flex space-x-5'>
-                <div className={`tabs ${tabs === 0 ? 'active' : ''}`} onClick={() => {handleActive(0)}}>หน้าหลัก <div className='underline-c'></div></div>
-                <div className={`tabs ${tabs === 1 ? 'active' : ''}`} onClick={() => {handleActive(1)}}>เสื้อผ้า <div className='underline-c'></div></div>
-                <div className={`tabs ${tabs === 2 ? 'active' : ''}`} onClick={() => {handleActive(2)}}>เครื่องประดับ <div className='underline-c'></div></div>
-                <div className={`tabs ${tabs === 3 ? 'active' : ''}`} onClick={() => {handleActive(3)}}>ของเล่น <div className='underline-c'></div></div>
+            <ul className='flex space-x-10'>
+                <div className={`tabs ${tabs === 0 ? 'active' : ''}`} onClick={() => {handleActive(0)}}>หน้าหลัก <div className={`underline-c ${tabs === 0 ? 'active' : ''}`}></div></div>
+                <div className={`tabs ${tabs === 1 ? 'active' : ''}`} onClick={() => {handleActive(1)}}>เสื้อผ้า <div className={`underline-c ${tabs === 1 ? 'active' : ''}`}></div></div>
+                <div className={`tabs ${tabs === 2 ? 'active' : ''}`} onClick={() => {handleActive(2)}}>เครื่องประดับ <div className={`underline-c ${tabs === 2 ? 'active' : ''}`}></div></div>
+                <div className={`tabs ${tabs === 3 ? 'active' : ''}`} onClick={() => {handleActive(3)}}>ของเล่น <div className={`underline-c ${tabs === 3 ? 'active' : ''}`}></div></div>
             </ul>
         </div>
         <div className="topnav-right space-x-5">
-            <button className='border-2 rounded-2xl px-5 py-1 '>SignIn</button>
-            <FaShoppingCart className='text-2xl my-auto'/>
+            <button className='border-2 rounded-3xl px-7 py-2 font-bold text-white bg-orange-500 hover:bg-orange-700'>Sign In</button>
+            <FaShoppingCart className='cart-icon text-2xl my-auto'/>
         </div>
     </nav>
   )
